dfde2c0b889b0c0d0cd48056f39833677c66e79e
Jose131103 <jsd0014@alu.medac.es>
2024-10-17 18:19:42 +0200

    update

--- Relacion2/ej7/code.js
+++ Relacion2/ej7/code.js
@@ -1,13 +1,14 @@
 window.onload = () =>{
-    let colors = ["black","red","blue","yellow"];
+    let colors = ["","black","red","blue","yellow"];
     let i;
     let dedos = document.getElementsByTagName("td");
     for (const element of dedos) {
-        i = 0;
+        
         element.onclick = function(){
-            this.style.backgroundColor = colors[i];
-            if(i<colors.length-1) i++;
-            else i=0;
+            i = colors.indexOf(this.style.backgroundColor);
+            console.log(i);
+            if(i != colors.length - 1) this.style.backgroundColor = colors[i+1];             
+            else this.style.backgroundColor = colors[1];
         }    
     }
 }
